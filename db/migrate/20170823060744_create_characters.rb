51f723ef401f0c12ea2f9753c348a6e06c1deeb6
Ruan Felipe D. Pontes <sirashtarsheran@gmail.com>
2017-08-23 03:25:02 -0300

    Start game project

---
+++ db/migrate/20170823060744_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
